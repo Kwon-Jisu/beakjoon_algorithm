b8a9e3a9861238acd942569141c6360825886a03
Kwon <wltn80609@ajou.ac.kr>
2025-01-14 10:35:05 +0900

    17626 코드수정

--- beakjoon_level_3/0110.py
+++ beakjoon_level_3/0110.py
@@ -5,18 +5,18 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-sq = [0 if i**0.5%1 else 1 for i in range(N + 1)]  # 제곱수는 1로 저장
+sq = [0 if i**0.5%1 else 1 for i in range(N + 1)]
 
 count = 4
 for i in range(int(N ** 0.5), 0, -1):
-    if sq[N]:  # n이 제곱수일 경우
+    if sq[N]:
         count = 1
         break
-    elif sq[N - i ** 2]:  # 나머지가 제곱수일 경우
+    elif sq[N - i ** 2]:
         count = 2
         break
     else:
         for j in range(int((N - i ** 2) ** 0.5), 0, -1):
-            if sq[(N - i ** 2) - j ** 2]:  # 제곱수를 한번 더 뺀 나머지가 제곱수일 경우
+            if sq[(N - i ** 2) - j ** 2]:
                 count = 3
 print(count)
